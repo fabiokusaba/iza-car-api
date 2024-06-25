bb34fcfae9e667d66a9f38f543c31083d404795f
Fabio Kusaba <fabinhokb44@gmail.com>
2024-06-25 19:14:56 -0300

    Initial commit

---
+++ src/main/java/tech/iza/car/IzaCarApiApplication.java
@@ -0,0 +1,13 @@
+package tech.iza.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzaCarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzaCarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/iza/car/IzaCarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.iza.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzaCarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
