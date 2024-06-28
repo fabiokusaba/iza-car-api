3f565e4d81b41415af00a7b400c8e90bd27acf13
Fabio Kusaba <fabinhokb44@gmail.com>
2024-06-28 20:09:38 -0300

    Add custom query

--- src/main/java/tech/iza/car/repository/ModeloRepository.java
+++ src/main/java/tech/iza/car/repository/ModeloRepository.java
@@ -3,5 +3,10 @@ package tech.iza.car.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.iza.car.model.modelo.ModeloEntity;
 
+import java.util.List;
+
 public interface ModeloRepository extends JpaRepository<ModeloEntity, Integer> {
+
+    //Buscando todos os modelos de uma marca específica
+    List<ModeloEntity> findByMarca(Integer marca);
 }
