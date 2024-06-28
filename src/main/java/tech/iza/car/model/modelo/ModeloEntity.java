b8a4979127a1e7a763fc0d766e3fb4691ae0414c
Fabio Kusaba <fabinhokb44@gmail.com>
2024-06-28 20:08:43 -0300

    Add marca relationship

--- src/main/java/tech/iza/car/model/modelo/ModeloEntity.java
+++ src/main/java/tech/iza/car/model/modelo/ModeloEntity.java
@@ -22,4 +22,15 @@ public class ModeloEntity {
     //Essa coluna vai representar a nossa exclusão lógica de Modelo, podemos pensar da seguinte forma o Modelo foi
     //excluído? Sim ou não
     private boolean excluido;
+
+    //Quando falamos de relacionamento entre tabelas é muito comum ouvirmos falar das anotações '@ManyToOne',
+    //'@OneToMany', '@ManoToMany', '@OneToOne', e precisamos tomar muito cuidado com tais anotações é claro que quando
+    //você retornar o Modelo é preciso saber a qual Marca ele pertence, mas às vezes não precisamos ter a Marca já logo
+    //identificada na iteração de consulta, são questionamentos que precisamos realizar quando estamos realizando um
+    //mapeamento com foco no nosso domínio
+
+    //Então, a primeira abordagem é que eu sei que preciso da Marca e que o Modelo tem uma relação com Marca, assim
+    //podemos usar a relação de id, valor inteiro, o id daquela marca isso seria o mínimo necessário que preciso
+    @Column(name = "marca_id")
+    private Integer marca;
 }
