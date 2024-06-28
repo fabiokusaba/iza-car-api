02abe727f380a7ff98817d51368574051511ac72
Fabio Kusaba <fabinhokb44@gmail.com>
2024-06-28 19:47:50 -0300

    Update method and return

--- src/main/java/tech/iza/car/webservice/ModeloResource.java
+++ src/main/java/tech/iza/car/webservice/ModeloResource.java
@@ -38,7 +38,16 @@ public class ModeloResource {
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Integer id) {
+    public Response delete(@PathVariable("id") Integer id) {
+
+        //A exclusão pode ser considerada tanto física quanto lógica, quando é uma exclusão física literalmente o
+        //registro é removido do banco de dados, no meio convencional no ambiente corporativo é muito comum que a
+        //gente pratique a exclusão lógica que basicamente é inativar ou sinalizar que aquele arquivo foi excluído
+        //então ao invés da gente realizar um delete a gente vai realizar um update em uma coluna que determine
+        //essa característica
+
         service.excluir(id);
+
+        return ResponseFactory.ok(true, "Modelo excluído com sucesso");
     }
 }
