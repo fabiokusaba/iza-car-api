a3c01257166395c71c5a4c88e84f32ac7b18ca3d
Fabio Kusaba <fabinhokb44@gmail.com>
2024-06-27 21:24:44 -0300

    Create modelo response

---
+++ src/main/java/tech/iza/car/model/modelo/ModeloResponse.java
@@ -0,0 +1,12 @@
+package tech.iza.car.model.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ModeloResponse {
+
+    private Integer id;
+    private String nome;
+}
