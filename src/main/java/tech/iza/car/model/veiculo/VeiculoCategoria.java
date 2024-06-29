d3e55c9da39823938623c633dede82b4ec84b712
Fabio Kusaba <fabinhokb44@gmail.com>
2024-06-28 21:48:22 -0300

    Create vehicle category

---
+++ src/main/java/tech/iza/car/model/veiculo/VeiculoCategoria.java
@@ -0,0 +1,10 @@
+package tech.iza.car.model.veiculo;
+
+public enum VeiculoCategoria {
+
+    //Quando estamos trabalhando com enums o índice inicia-se em zero
+    A, //0
+    B, //1
+    C, //2
+    D //3
+}
